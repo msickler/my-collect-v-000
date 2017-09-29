8da9cff947489bf2de3fe4d970c1f51fcf43255f
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-29 00:06:35 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,10 +2,8 @@ def my_collect(array)
   i = 0
   collection = []
   while i < array.length
-   my_collect(array) do |lang|
-    lang.upcase
-  end
-    i += 1
+  yield (array[i]) 
+      i += 1
   end
   collection
 end
